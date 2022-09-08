79a4d0dedb6ecfbd31f60b9d5e3e62c3639ebbc8
Ana Juárez <lisa13suarez@gmail.com>
2022-09-07 22:50:06 -0600

    edit css - colors and title of header page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
 
   return (
     <div className='App'>
-      <h1 className='titleApp'>♦ User Api ♦</h1>
+      <h1 className='titleApp'>User Random <br/><span> By Ana Juárez</span></h1>
+      
       <div className='container-gb-square'>
         <div className='square1 square'></div>
         <div className='square2 square'></div>
