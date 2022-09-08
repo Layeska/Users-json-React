6a9b1f84b2c3086b678fd69e4590ba444b1d31d9
Ana Juárez <lisa13suarez@gmail.com>
2022-09-07 22:26:44 -0600

    add responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className='App'>
-      <h1>♦ User Api ♦</h1>
+      <h1 className='titleApp'>♦ User Api ♦</h1>
       <div className='container-gb-square'>
         <div className='square1 square'></div>
         <div className='square2 square'></div>
